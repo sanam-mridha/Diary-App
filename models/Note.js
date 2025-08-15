7db5b05a84b6d4cef44c2aa4615755e379125095
Unfucked Account Of Sanam <221183806+sanam-mridha@users.noreply.github.com>
2025-08-16 00:07:42 +0600

    Update Note.js

--- models/Note.js
+++ models/Note.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const NoteSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  isSecret: { type: Boolean, default: false },
+  encryptedContent: { type: String }, 
+  passwordHash: { type: String },     
+}, { timestamps: true });
+
+module.exports = mongoose.model('Note', NoteSchema);
