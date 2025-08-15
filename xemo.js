0bce3832b5410948a944ab5824cce541465d01c6
Unfucked Account Of Sanam <221183806+sanam-mridha@users.noreply.github.com>
2025-08-16 00:24:06 +0600

    Update xemo.js

--- xemo.js
+++ xemo.js
@@ -9,7 +9,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 dotenv.config();
 const app = express();
-
+app.use(express.static('view'));
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(helmet());
