66df9c017aa40c575497ef000f20e60d305caf1d
Unfucked Account Of Sanam <221183806+sanam-mridha@users.noreply.github.com>
2025-08-16 00:08:13 +0600

    Update encryption.js

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,1 +1,22 @@
+const CryptoJS = require('crypto-js');
+const bcrypt = require('bcryptjs');
 
+function encrypt(content, password) {
+  return CryptoJS.AES.encrypt(content, password).toString();
+}
+
+function decrypt(cipherText, password) {
+  const bytes = CryptoJS.AES.decrypt(cipherText, password);
+  return bytes.toString(CryptoJS.enc.Utf8);
+}
+
+async function hashPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+}
+
+async function comparePassword(password, hash) {
+  return await bcrypt.compare(password, hash);
+}
+
+module.exports = { encrypt, decrypt, hashPassword, comparePassword };
